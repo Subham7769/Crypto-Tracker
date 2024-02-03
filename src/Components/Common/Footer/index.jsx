f6dcad539e0a24df03d577eef0a0d2c0ec0547dd
Shubham Jain <shubham.rapariya2@gmail.com>
2024-02-04 00:26:17 +0530

    bug fixes

--- src/Components/Common/Footer/index.jsx
+++ src/Components/Common/Footer/index.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./style.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import EmailIcon from "@mui/icons-material/Email";
-import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
   function topFunction() {
@@ -16,14 +17,14 @@ function Footer() {
         CryptoTracker<span>.</span>
       </h2>
       <div className="social-links">
-        <a href="https://facebook.com">
-          <FacebookIcon className="social-link" />
+        <a href="https://github.com/Subham7769">
+          <GitHubIcon className="social-link" />
         </a>
         <a href="mailto:shubham.rapariya2@gmail.com">
           <EmailIcon className="social-link" />
         </a>
-        <a href="https://www.instagram.com">
-          <InstagramIcon className="social-link" />
+        <a href="https://www.linkedin.com/in/shubhamjain7769/">
+          <LinkedInIcon className="social-link" />
         </a>
       </div>
     </div>
