c1fa01e632fff602a334fe28d7d8a4f890f65138
Shubham Jain <shubham.rapariya2@gmail.com>
2024-02-04 00:15:49 +0530

    bug fixes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from '../Components/Common/Header'
 import LandingPage from '../Components/LandingPage/'
-import Footer from '../components/Common/Footer'
+
 
 const HomePage = () => {
   return (
     <section>
         <Header/>
         <LandingPage/>
-        <Footer/>
+
     </section>
   )
 }
