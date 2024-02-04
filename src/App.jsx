408d3d4f38d9c6e52ceba2b17c736ea02462a2ca
Shubham Jain <shubham.rapariya2@gmail.com>
2024-02-04 20:29:48 +0530

    trying to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Route,Routes } from 'react-router-dom';
 import React from 'react'
 import HomePage from './pages/HomePage.jsx';
-import ComparePage from './pages/ComparePage.jsx';
+// import ComparePage from './pages/ComparePage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
-// import CoinPage from './pages/CoinPage.jsx';
+import CoinPage from './pages/CoinPage.jsx';
 import WatchlistPage from './pages/WatchlistPage.jsx';
 
 
@@ -12,9 +12,9 @@ const App = () => {
     <div>
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
-        <Route path='/Compare' element={<ComparePage/>}></Route>
+        {/* <Route path='/Compare' element={<ComparePage/>}></Route> */}
         <Route path='/Dashboard' element={<DashboardPage/>}></Route>
-        {/* <Route path='/Coin/:id' element={<CoinPage/>}></Route> */}
+        <Route path='/Coin/:id' element={<CoinPage/>}></Route>
         <Route path='/Watchlist' element={<WatchlistPage/>}></Route>
       </Routes>
     </div>
